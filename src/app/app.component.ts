4aaca222bc91ebe25e7a5214a24b390e9aa01c3b
M Rafiul Alam Tonmoy <mrafiul.alam7@gmail.com>
2023-11-30 06:54:08 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { COURSES } from './interfaces/courses.interface';
+import { Courses } from './core/course.db';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'test';
+  courses: COURSES[] = Courses;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { SwiperModule } from 'swiper/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedCardComponent } from './shared/shared-card/shared-card.component';
+import { TestComponent } from './pages/test/test.component';
+import { SwipperCarouselComponent } from './shared/swipper-carousel/swipper-carousel.component';
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, SharedCardComponent, TestComponent, SwipperCarouselComponent],
+  imports: [BrowserModule, AppRoutingModule, SwiperModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
